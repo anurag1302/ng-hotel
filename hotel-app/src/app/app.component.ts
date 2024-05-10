8ff73a677c1ab50f11831f6d06ed6675461180f6
Anurag <anurag3487@gmail.com>
2024-05-11 00:37:45 +0530

    ng - temp;ateUrl & styleUrl story

--- hotel-app/src/app/app.component.ts
+++ hotel-app/src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  //template:'<h1>Hey there</h1>', -- inline html can be written here
+  // template:`<h1>I like pizza</h1> -- multi line inline html can be written using tilde
+  // <p>Margarita pizza is best</p>
+  // `,
+  styleUrl: './app.component.scss',
+  //styles:[`h1{color:red;}`] -- inline styles are also supported
 })
 export class AppComponent {
   title = 'hotel-app';
